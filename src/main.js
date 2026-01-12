89d563df87cfec084009bc76f4fbf714b819826f
Benzeminho <pierre.cescosse@icloud.com>
2026-01-12 14:17:16 +0100

    testtrufflehog7

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.js';
 
+
 document.querySelector('#app').innerHTML = `
   <div>
+    <div>hello world!</div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
+    <h1>Salut</h1>
+    <div class="card"
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
